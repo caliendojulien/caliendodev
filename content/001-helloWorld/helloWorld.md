4827141f0f594cc9b12d69acd37883163c3e181f
Caliendo Julien <caliendo@hotmail.fr>
2021-08-22 01:09:34 +0200

    Commit initial

---
+++ content/001-helloWorld/helloWorld.md
@@ -0,0 +1,9 @@
+Title: Hello World
+Date: 2021-08-22 10:30
+Category: blog
+Tags: pelican, blog, hello, world
+Slug: hello-world
+Authors: Caliendo Julien
+Summary: Hello World
+
+Premier article du blog.
---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Caliendo Julien'
+SITENAME = 'CaliendoDev'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('Pelican', 'https://getpelican.com/'),
+    ('Python.org', 'https://www.python.org/'),
+    ('Jinja2', 'https://palletsprojects.com/p/jinja/')
+)
+
+# Social widget
+SOCIAL = (('Twitter', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+GITHUB_URL = 'http://github.com/caliendojulien/'
